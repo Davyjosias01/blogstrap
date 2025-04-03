4bd8bbe3f551cd46b3a091f14052c24f1eefd0b6
Davy Josias <davy.josias@gmail.com>
2025-04-03 17:31:02 -0300

    10:44 da aula 6

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,27 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = Article.all
+    @articles = Article.all;
   end
 
   def show
-    @article = Article.find(params[:id])
+    @article = Article.find(params[:id]);
   end
+
+  def new
+    @article = Article.new;
+  end
+  
+  def create 
+    @article = Article.new();
+
+    if @article.save
+      redirect_to @article
+    else 
+      render :new
+    end
+
+
+  end
+
 
 end
