d70c57442ad9d36118e454764f0420bc77e94745
Davy Josias <davy.josias@gmail.com>
2025-04-15 17:31:16 -0300

    Commit de final do dia

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,13 +1,3 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
-
-const path = require('path')
-
-environment.config.merge({
-  entry: {
-    application: path.resolve(__dirname, '../../app/javascript/packs/application.js')
-  }
-})
-
-module.exports = environment
